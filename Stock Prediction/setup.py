8876167589f12c6ad501754585d48657a4aada8e
Ivory Le <126025086+Ivoryle82@users.noreply.github.com>
2024-05-21 14:56:06 +0700

    Initialized the repo with cookiecutter data science

---
+++ Stock Prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will use Support Vectore Machines in order to predict the Stock Price direction',
+    author='Ivory Le',
+    license='',
+)
